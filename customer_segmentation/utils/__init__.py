968cda25559ee46f6319675c80f9ede90c98555a
Tanwar-Aastha <tanwaraastha89@gmail.com>
2024-03-25 10:00:00 +0530

    model training

--- customer_segmentation/utils/__init__.py
+++ customer_segmentation/utils/__init__.py
@@ -3,6 +3,10 @@ import sys
 import pickle
 from customer_segmentation.exception.exception import CustomerSegmentationException
 from customer_segmentation.logging.logger import logging
+from sklearn.model_selection import GridSearchCV
+
+
+from sklearn.metrics import accuracy_score
 
 
 def save_objects_file(file_path, object):
@@ -28,4 +32,38 @@ def load_obj_file(file_path):
         logging.error(str(e))
         raise CustomerSegmentationException(e, sys)   
     
+def evaluate_model(x_train, y_train, x_test, y_test, models, params):
+    """
+    This function will return a dictionary of models with their accuracy score as values.
+    """
+    try:
+        report = {}
+        logging.info("Starting the model fitting and accuracy score calculation")
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(x_train, y_train)
+
+            model.set_params(**gs.best_params_)
+
+            model.fit(x_train, y_train)
+            y_train_pred = model.predict(x_train)
+            y_test_pred = model.predict(x_test)
+            logging.info("Predicted the y test")
+
+            train_model_score = accuracy_score(y_train, y_train_pred)
+            test_model_score = accuracy_score(y_test, y_test_pred)
+            logging.info(f"Calculated the accuracy score for {list(models.keys())[i]} model")
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+
+
+    except Exception as e:
+        logging.error(str(e))
+        raise CustomerSegmentationException(e, sys)
    
