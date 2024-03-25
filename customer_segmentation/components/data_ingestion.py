754e071af5f7a913f628477f76f0522cdc2f8776
Tanwar-Aastha <tanwaraastha89@gmail.com>
2024-03-25 10:00:00 +0530

    Data Transformation

--- customer_segmentation/components/data_ingestion.py
+++ customer_segmentation/components/data_ingestion.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from customer_segmentation.logging import logger
+from customer_segmentation.logger import logger
 from customer_segmentation.exception import exception
 import pandas as pd
 from dataclasses import dataclass
@@ -24,19 +24,18 @@ class DataIngestion:
 
 
     def initiate_data_ingestion(self):
-        logger.logging.info("Entered the data ingestion component")
+        logger.logger.info("Entered the data ingestion component")
         try:
             df = pd.read_csv("D:\Aastha\Projects\Customer_dispute_Segmentation\Customer_data\data.csv")
-            logger.logging.info("Read the Data as a DataFrame")
+            logger.logger.info("Read the Data as a DataFrame")
 
             # creating the artifact directory
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             # loading the data into raw data path
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            logger.logging.info("Data uploaded in raw data path")
+            logger.logger.info("Data uploaded in raw data path")
 
-            # split the data into train and test set
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
         except:
             pass
